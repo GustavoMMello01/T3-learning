6075ba6c0c8fbd9e94c4a72f545e0cfb9da52551
Gustavo <gustavommello03@gmail.com>
2023-01-02 23:34:04 +0100

    find builds started

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -12,9 +12,14 @@ export const Navbar = () => {
           <ul
             className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a href="/"
+              <a href=""
                  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                  aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="/find-builds"
+                 className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                 aria-current="page">Find Builds</a>
             </li>
             <li>
               <a href="/submit-build"
